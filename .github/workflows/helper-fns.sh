22baa8ae1db8c6b4711757f091f92598ea2c88fc
Izaak Beekman <zbeekman@gmail.com>
2020-02-03 20:55:08 -0500

    Change to safer single push to gh-pages

--- .github/workflows/helper-fns.sh
+++ .github/workflows/helper-fns.sh
@@ -5,28 +5,28 @@
 code () {
     # Read from STDIN and wrap contents in GFM codeblock demarcation
     # Takes an optional argument of a file name to override where output is concatenated (default: gh-pages/README.md)
-    printf '\n```\n' >> "${1:-gh-pages/README.md}"
+    printf '\n```\n' >> "${1:-${DOC:-gh-pages/README.md}}"
     while IFS= read -r line; do
-        echo "$line" >> "${1:-gh-pages/README.md}"
+        echo "$line" >> "${1:-${DOC:-gh-pages/README.md}}"
     done
-    printf '```\n\n' >> "${1:-gh-pages/README.md}"
+    printf '```\n\n' >> "${1:-${DOC:-gh-pages/README.md}}"
 }
 
 twocol () {
     # Print a GFM table header with two columns, the first of which is right-aligned
-    printf '\n| key | value |\n| ---: | --- |\n' >> "${1:-gh-pages/README.md}"
+    printf '\n| key | value |\n| ---: | --- |\n' >> "${1:-${DOC:-gh-pages/README.md}}"
 }
 
 h () {
     # Print a markdown header with the level specified by an integer.
     head_symbols='#################################'
     head="${head_symbols:0:${1:-1}}"
-    printf '\n%s %s\n\n' "$head" "$2" >> "${3:-gh-pages/README.md}"
+    printf '\n%s %s\n\n' "$head" "$2" >> "${3:-${DOC:-gh-pages/README.md}}"
 }
 
 to_list () {
-    printf '\n' >> "${1:-gh-pages/README.md}"
+    printf '\n' >> "${1:-${DOC:-gh-pages/README.md}}"
     while read -r line; do
-        printf '  * %s\n' "$line" >> "${1:-gh-pages/README.md}"
+        printf '  * %s\n' "$line" >> "${1:-${DOC:-gh-pages/README.md}}"
     done
 }
